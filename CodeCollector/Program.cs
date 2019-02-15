5c301d0119d9b5be3309179fd4c4285afea4a370
Oscar Davies <oscar.william.davies@outlook.com>
2019-02-15 21:26:49 +0000

    Some commenting Pog

--- CodeCollector/Program.cs
+++ CodeCollector/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeCollector
 {
     class Program
     {
-        static List<string> FileExt = new List<string> { "cs" },
-            PathExceptions = new List<string> { "bin","obj", "Properties" };
+        static List<string> FileExt = new List<string> { ".cs" }, //File extensions that will be placed in the Collection
+            PathExceptions = new List<string> { "bin","obj", "Properties" };//Folder paths that wont be traversed
         static void Main(string[] args)
         {
             Dictionary<string,string> CodeCollection = GetCodeCollection("D:/Programming/ComputerScienceCoursework/Code/Twitch-Discord-Reward-Bot/Twitch-Discord-Reward-Bot/Twitch-Discord-Reward-Bot");
@@ -21,22 +18,24 @@ namespace CodeCollector
         static void WriteCodeCollectionToFile(Dictionary<string,string> CodeCollection,string FilePath)
         {
             string FileContents="";
+            //Add every File path and its corresponding contents into the cumilative file content
             foreach (KeyValuePair<string,string> CodeFilePair in CodeCollection)
             {
                 FileContents += CodeFilePair.Key + "\n\n" + CodeFilePair.Value + "\n";
             }
-            System.IO.File.WriteAllText(FilePath,FileContents);
+            System.IO.File.WriteAllText(FilePath,FileContents);//Write it to the fucking file
         }
 
         static Dictionary<string,string> GetCodeCollection(string DirectoryPath)
         {
-            Dictionary<string, string> FileCodePairs = new Dictionary<string, string> { };
+            Dictionary<string, string> FileCodePairs = new Dictionary<string, string> { };//Stores the file path along with the files contents
             TraverseDirectoryFolders(DirectoryPath,DirectoryPath,FileCodePairs);
             return FileCodePairs;
         }
 
         static void TraverseDirectoryFolders(string DirectoryPath,string SourcePath, Dictionary<string, string> FileCodePairs)
         {
+            //Traverse Every Child Directory Where the Path doesnt contain an excluded path
             foreach (string ChildPaths in System.IO.Directory.GetDirectories(DirectoryPath).Where(x=>PathExceptions.Where(y=>x.Contains(y)).Count()==0))
             {
                 TraverseDirectoryFolders(ChildPaths, SourcePath, FileCodePairs);
@@ -46,8 +45,10 @@ namespace CodeCollector
 
         static void TraverseDirectoryFiles(string DirectoryPath, string SourcePath, Dictionary<string, string> FileCodePairs)
         {
+            //Add every file in the directory where the file extension matches a valid file extension
             foreach (string FilePath in System.IO.Directory.GetFiles(DirectoryPath).Where(x=>FileExt.Where(y=>x.EndsWith(y)).Count()!=0))
             {
+                //This improves the dumped files beauty, and reduces space used if pasted into word
                 FileCodePairs.Add(FilePath.Remove(0,SourcePath.Length+1).Replace("\\","/"), System.IO.File.ReadAllText(FilePath).Replace("\r\n","\r"));
             }
         }
